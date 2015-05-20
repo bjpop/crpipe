2a1cb65092ee88b90bc9776f42b5f34f32b0da9b
Bernie Pope <florbitous@gmail.com>
2015-05-20 12:13:40 +1000

    Renamed crpipe.py to main.py

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ setup(
     packages=['crpipe'],
     package_data={'crpipe': ['examples/*.py']},
     entry_points={
-        'console_scripts': ['crpipe = crpipe.crpipe:main']
+        'console_scripts': ['crpipe = crpipe.main:main']
     },
     url='https://github.com/bjpop/crpipe',
     license='LICENSE.txt',
     description='crpipe is a pipeline system for bioinformatics workflows\
      with support for running pipeline stages on a distributed compute cluster.',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[
         "ruffus == 2.6.3",
         "drmaa == 0.7.6",

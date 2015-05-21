8a58772b4aa69a7ea9c4185b581b524965d312ca
Bernie Pope <bjpope@unimelb.edu.au>
2015-05-22 09:43:54 +1000

    Make stage functions methods of a class

--- src/pipeline.py
+++ src/pipeline.py
@@ -3,48 +3,40 @@ Build the pipeline workflow by plumbing the stages together.
 '''
 
 from ruffus import Pipeline, suffix, formatter, add_inputs
-from stages import (make_stage, fastqc, index_reference_bwa, 
-    index_reference_samtools, align_bwa)
+from stages import Stages
 
 
-def make_pipeline(config, state):
+def make_pipeline(state):
     '''Build the pipeline by constructing stages and connecting them together'''
     # Build an empty pipeline
     pipeline = Pipeline(name="crpipe")
     # Get a list of paths to all the FASTQ files
-    fastq_files = config.get_option('fastqs')
+    fastq_files = state.config.get_option('fastqs')
+
+    stages = Stages(state)
 
     # Run fastQC on the FASTQ files
-    pipeline.transform(task_func=make_stage(state, fastqc),
+    pipeline.transform(task_func=stages.fastqc,
         input=fastq_files,
         filter=suffix('.fastq.gz'),
         output='_fastqc')
 
-    reference_file = config.get_option('reference')
+    reference_file = state.config.get_option('reference')
 
     # Index the reference using BWA 
-    pipeline.transform(task_func=make_stage(state, index_reference_bwa),
+    pipeline.transform(task_func=stages.index_reference_bwa,
         input=reference_file,
         filter=suffix('.fa'),
         output=['.fa.amb', '.fa.ann', '.fa.pac', '.fa.sa', '.fa.bwt'])
     
     # Index the reference using samtools 
-    pipeline.transform(task_func=make_stage(state, index_reference_samtools),
+    pipeline.transform(task_func=stages.index_reference_samtools,
         input=reference_file,
         filter=suffix('.fa'),
         output='.fa.fai')
 
-    '''
-    Could write like so, by passing state in extra parameters
-    pipeline.transform(task_func=index_reference_samtools,
-        input=reference_file,
-        filter=suffix('.fa'),
-        output='.fa.fai',
-        extras=[state])
-    '''
-
     # Align paired end reads in FASTQ to the reference producing a BAM file
-    pipeline.transform(task_func=make_stage(state, align_bwa),
+    pipeline.transform(task_func=stages.align_bwa,
         input=fastq_files,
         # Match the R1 (read 1) FASTQ file and grab the path and sample name. 
         # This will be the first input to the stage.
